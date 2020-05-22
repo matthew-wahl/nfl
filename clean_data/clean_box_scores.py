e59c9f49838338914023a73c800d5e7aaeba2c20
matthew-wahl <matthew.wahl@ryerson.ca>
2020-05-22 10:25:08 -0400

    fix rav, clt title bug

--- clean_data/clean_box_scores.py
+++ clean_data/clean_box_scores.py
@@ -25,7 +25,8 @@ nfl = get_list_of_files('C:/Users/matth/PycharmProjects/untitled1/nfl/box_scores
 team_list = pd.read_csv('C:/Users/matth/PycharmProjects/untitled1/team_abbreviations_list.csv',
                         header=None).values.tolist()
 
-p_cols = ['Week', 'Home', 'Cmp', 'Att', 'Yds', '1D', 'IAY', 'CAY', 'YAC', 'Drops', 'BadTh', 'Sk', 'Bltz', 'Hrry',
+p_cols = ['Year', 'Week', 'Home', 'Cmp', 'Att', 'Yds', '1D', 'IAY', 'CAY', 'YAC', 'Drops', 'BadTh', 'Sk', 'Bltz',
+          'Hrry',
           'Hits',
           'Scrm', 'ScrambleYds', 'Att', 'Yds', '1D', 'YBC', 'YAC', 'BrkTkl', 'Tgt', 'Rec', 'Yds', '1D', 'YBC', 'YAC',
           'BrkTkl', 'Drop', 'Int', 'Tgt', 'Cmp', 'Yds', 'TD', 'Rat', 'Air', 'YAC', 'Bltz', 'Hrry',
@@ -139,12 +140,13 @@ for file in nfl:
         else:
             home = 1
 
-        totals = [week_num, home]
+        totals = [year, week_num, home]
         for col in cols:
             totals.append(passing[col].sum())
 
-        cols.insert(0, 'Week')
-        cols.insert(1, 'Home')
+        cols.insert(0, 'Year')
+        cols.insert(1, 'Week')
+        cols.insert(2, 'Home')
         df_passing = pd.DataFrame([totals], columns=cols)
 
         # rushing
@@ -197,69 +199,67 @@ for file in nfl:
             break
 
 # write team box score lists to CSVs
-sdg_df = pd.DataFrame(SDG, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/JAX.CSV', index=False)
-rai_df = pd.DataFrame(RAI, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/RAI.CSV', index=False)
-phi_df = pd.DataFrame(PHI, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/PHI.CSV', index=False)
-dal_df = pd.DataFrame(DAL, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/DAL.CSV', index=False)
-nyg_df = pd.DataFrame(NYG, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/NYG.CSV', index=False)
-was_df = pd.DataFrame(WAS, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/WAS.CSV', index=False)
-gnb_df = pd.DataFrame(GNB, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/GNB.CSV', index=False)
-min_df = pd.DataFrame(MIN, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/MIN.CSV', index=False)
-chi_df = pd.DataFrame(CHI, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/CHI.CSV', index=False)
-det_df = pd.DataFrame(DET, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/DET.CSV', index=False)
-sfo_df = pd.DataFrame(SFO, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/SFO.CSV', index=False)
-nor_df = pd.DataFrame(NOR, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/NOR.CSV', index=False)
-atl_df = pd.DataFrame(ATL, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/ATL.CSV', index=False)
-car_df = pd.DataFrame(CAR, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/CAR.CSV', index=False)
-sea_df = pd.DataFrame(SEA, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/SEA.CSV', index=False)
-ram_df = pd.DataFrame(RAM, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/RAM.CSV', index=False)
-tam_df = pd.DataFrame(TAM, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/TAM.CSV', index=False)
-crd_df = pd.DataFrame(CRD, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/CRD.CSV', index=False)
-nwe_df = pd.DataFrame(NWE, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/NWE.CSV', index=False)
-buf_df = pd.DataFrame(BUF, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/BUF.CSV', index=False)
-nyj_df = pd.DataFrame(NYJ, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/NYJ.CSV', index=False)
-mia_df = pd.DataFrame(MIA, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/MIA.CSV', index=False)
-rav_df = pd.DataFrame(RAV, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/RAV.CSV', index=False)
-pit_df = pd.DataFrame(PIT, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/PIT.CSV', index=False)
-cle_df = pd.DataFrame(CLE, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/CLE.CSV', index=False)
-cin_df = pd.DataFrame(CIN, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/CIN.CSV', index=False)
-htx_df = pd.DataFrame(HTX, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/HTX.CSV', index=False)
-oti_df = pd.DataFrame(OTI, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/OTI.CSV', index=False)
-clt_df = pd.DataFrame(CLT, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/CLT.CSV', index=False)
-jax_df = pd.DataFrame(JAX, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/JAX.CSV', index=False)
-kan_df = pd.DataFrame(KAN, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/KAN.CSV', index=False)
-den_df = pd.DataFrame(DEN, columns=p_cols).to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl'
-                                                  '/team_box_scores_sorted/DEN.CSV', index=False)
-
-
+sdg_df = pd.DataFrame(SDG, columns=p_cols).sort_values(by=['Year', 'Week'])
+sdg_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/SDG.CSV', index=False)
+rai_df = pd.DataFrame(RAI, columns=p_cols).sort_values(by=['Year', 'Week'])
+rai_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/RAI.CSV', index=False)
+phi_df = pd.DataFrame(PHI, columns=p_cols).sort_values(by=['Year', 'Week'])
+phi_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/PHI.CSV', index=False)
+dal_df = pd.DataFrame(DAL, columns=p_cols).sort_values(by=['Year', 'Week'])
+dal_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/DAL.CSV', index=False)
+nyg_df = pd.DataFrame(NYG, columns=p_cols).sort_values(by=['Year', 'Week'])
+nyg_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/NYG.CSV', index=False)
+was_df = pd.DataFrame(WAS, columns=p_cols).sort_values(by=['Year', 'Week'])
+was_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/WAS.CSV', index=False)
+gnb_df = pd.DataFrame(GNB, columns=p_cols).sort_values(by=['Year', 'Week'])
+gnb_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/GNB.CSV', index=False)
+min_df = pd.DataFrame(MIN, columns=p_cols).sort_values(by=['Year', 'Week'])
+min_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/MIN.CSV', index=False)
+chi_df = pd.DataFrame(CHI, columns=p_cols).sort_values(by=['Year', 'Week'])
+chi_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/CHI.CSV', index=False)
+det_df = pd.DataFrame(DET, columns=p_cols).sort_values(by=['Year', 'Week'])
+det_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/DET.CSV', index=False)
+sfo_df = pd.DataFrame(SFO, columns=p_cols).sort_values(by=['Year', 'Week'])
+sfo_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/SFO.CSV', index=False)
+nor_df = pd.DataFrame(NOR, columns=p_cols).sort_values(by=['Year', 'Week'])
+nor_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/NOR.CSV', index=False)
+atl_df = pd.DataFrame(ATL, columns=p_cols).sort_values(by=['Year', 'Week'])
+atl_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/ATL.CSV', index=False)
+car_df = pd.DataFrame(CAR, columns=p_cols).sort_values(by=['Year', 'Week'])
+car_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/CAR.CSV', index=False)
+sea_df = pd.DataFrame(SEA, columns=p_cols).sort_values(by=['Year', 'Week'])
+sea_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/SEA.CSV', index=False)
+ram_df = pd.DataFrame(RAM, columns=p_cols).sort_values(by=['Year', 'Week'])
+ram_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/RAM.CSV', index=False)
+tam_df = pd.DataFrame(TAM, columns=p_cols).sort_values(by=['Year', 'Week'])
+tam_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/TAM.CSV', index=False)
+crd_df = pd.DataFrame(CRD, columns=p_cols).sort_values(by=['Year', 'Week'])
+crd_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/CRD.CSV', index=False)
+nwe_df = pd.DataFrame(NWE, columns=p_cols).sort_values(by=['Year', 'Week'])
+nwe_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/NWE.CSV', index=False)
+buf_df = pd.DataFrame(BUF, columns=p_cols).sort_values(by=['Year', 'Week'])
+buf_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/BUF.CSV', index=False)
+nyj_df = pd.DataFrame(NYJ, columns=p_cols).sort_values(by=['Year', 'Week'])
+nyj_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/NYJ.CSV', index=False)
+mia_df = pd.DataFrame(MIA, columns=p_cols).sort_values(by=['Year', 'Week'])
+mia_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/MIA.CSV', index=False)
+rav_df = pd.DataFrame(RAV, columns=p_cols).sort_values(by=['Year', 'Week'])
+rav_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/RAV.CSV', index=False)
+pit_df = pd.DataFrame(PIT, columns=p_cols).sort_values(by=['Year', 'Week'])
+pit_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/PIT.CSV', index=False)
+cle_df = pd.DataFrame(CLE, columns=p_cols).sort_values(by=['Year', 'Week'])
+cle_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/CLE.CSV', index=False)
+cin_df = pd.DataFrame(CIN, columns=p_cols).sort_values(by=['Year', 'Week'])
+cin_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/CIN.CSV', index=False)
+htx_df = pd.DataFrame(HTX, columns=p_cols).sort_values(by=['Year', 'Week'])
+htx_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/HTX.CSV', index=False)
+oti_df = pd.DataFrame(OTI, columns=p_cols).sort_values(by=['Year', 'Week'])
+oti_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/OTI.CSV', index=False)
+clt_df = pd.DataFrame(CLT, columns=p_cols).sort_values(by=['Year', 'Week'])
+clt_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/CLT.CSV', index=False)
+jax_df = pd.DataFrame(JAX, columns=p_cols).sort_values(by=['Year', 'Week'])
+jax_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/JAX.CSV', index=False)
+kan_df = pd.DataFrame(KAN, columns=p_cols).sort_values(by=['Year', 'Week'])
+kan_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/KAN.CSV', index=False)
+den_df = pd.DataFrame(DEN, columns=p_cols).sort_values(by=['Year', 'Week'])
+den_df.to_csv('C:/Users/matth/PycharmProjects/untitled1/nfl/team_box_scores_sorted/DEN.CSV', index=False)
